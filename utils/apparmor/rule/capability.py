0af81d75b31308388a25bcf6e54c50e4dae9c400
Samuel Sapalski <samuel.sapalski@nokia.com>
2016-03-02 16:05:05 +0100

    apply upstream patch for python2 str type comparison bug
    References:
    http://bazaar.launchpad.net/~apparmor-dev/apparmor/2.10/revision/3290

--- utils/apparmor/rule/capability.py
+++ utils/apparmor/rule/capability.py
@@ -15,7 +15,7 @@
 # ----------------------------------------------------------------------
 
 from apparmor.regex import RE_PROFILE_CAP
-from apparmor.common import AppArmorBug, AppArmorException
+from apparmor.common import AppArmorBug, AppArmorException, type_is_str
 from apparmor.rule import BaseRule, BaseRuleset, parse_modifiers
 import re
 
@@ -48,7 +48,7 @@ class CapabilityRule(BaseRule):
             self.all_caps = True
             self.capability = set()
         else:
-            if type(cap_list) == str:
+            if type_is_str(cap_list):
                 self.capability = {cap_list}
             elif type(cap_list) == list and len(cap_list) > 0:
                 self.capability = set(cap_list)
